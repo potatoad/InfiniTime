fabe9cd938e4261e62942dd7b1eb01f6e4d42324
potatoad <35428633+potatoad@users.noreply.github.com>
2022-04-25 15:49:31 +0100

    Update WatchFaceDigital.cpp

--- src/displayapp/screens/WatchFaceDigital.cpp
+++ src/displayapp/screens/WatchFaceDigital.cpp
@@ -69,7 +69,7 @@ WatchFaceDigital::WatchFaceDigital(DisplayApp* app,
   lv_label_set_long_mode(backgroundLabel, LV_LABEL_LONG_CROP);
   lv_obj_set_size(backgroundLabel, 240, 240);
   lv_obj_set_pos(backgroundLabel, 0, 0);
-  lv_label_set_text_static(backgroundLabel, "#55CDFC F##F7A8B8 I##FFFFFF N##F7A8B8 N##55CDFC !#");
+  lv_label_set_text(backgroundLabel, "#55CDFC F##F7A8B8 I##FFFFFF N##F7A8B8 N##55CDFC !#");
 
   heartbeatIcon = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_text_static(heartbeatIcon, Symbols::heartBeat);
