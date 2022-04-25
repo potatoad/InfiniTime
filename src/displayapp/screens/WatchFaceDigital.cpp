9fb0181505206c27bd2f464c1660f93f595ac45f
potatoad <35428633+potatoad@users.noreply.github.com>
2022-04-25 16:14:55 +0100

    Update WatchFaceDigital.cpp

--- src/displayapp/screens/WatchFaceDigital.cpp
+++ src/displayapp/screens/WatchFaceDigital.cpp
@@ -49,7 +49,7 @@ WatchFaceDigital::WatchFaceDigital(DisplayApp* app,
   notificationIcon = lv_label_create(lv_scr_act(), nullptr);
   lv_obj_set_style_local_text_color(notificationIcon, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0x00FF00));
   lv_label_set_text_static(notificationIcon, NotificationIcon::GetIcon(false));
-  lv_obj_align(notificationIcon, nullptr, LV_ALIGN_IN_TOP_LEFT, 0, 0);
+  lv_obj_align(notificationIcon, nullptr, LV_ALIGN_IN_TOP_LEFT, 0, 30);
 
   label_date = lv_label_create(lv_scr_act(), nullptr);
   lv_obj_align(label_date, lv_scr_act(), LV_ALIGN_CENTER, 0, 60);
